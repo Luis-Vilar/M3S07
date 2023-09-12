384f421a80a0d2e5e4d80befec844505946501e7
rafazamp <rafaelprimieri@hotmail.com>
2023-09-11 21:24:09 -0300

    Ajustado database.config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,14 +1,13 @@
 const { config } = require("dotenv");
 config();
 module.exports = {
-  dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
-  host: process.env.HOST, //Qual servidor está utilizando;
-  username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
-  password: process.env.PASSWORDDB, //Qual a senha do seu usuário no postgres;
-  database: process.env.DATABASE, //Qual o nome do seu database no postgres;
-  port: process.env.PORT, //Qual porta do seu postgres (Normalmente é a 5432);
+  dialect: process.env.DIALECT, 
+  host: process.env.HOST, 
+  username: process.env.USERNAMEDB, 
+  password: process.env.PASSWORDDB, 
+  database: process.env.DATABASE, 
+  port: process.env.PORT, 
   define: {
-    // O objeto define serve para colocarmos configurações de conexão auxiliares
     underscored: true, //Traduz os campos para snake_case
     underscoredAll: true, //Traduz todos os campos para snake_case
   },
