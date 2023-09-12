d841b41dea988364f6870cdf05f3fad0a1a5cf5e
rafazamp <rafaelprimieri@hotmail.com>
2023-09-12 14:35:18 -0300

    Removida a rota de usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const UserRoutes = require("./v1/user.routes");
-
-router.use([UserRoutes])
+//const UserRoutes = require("./v1/user.routes");
+//router.use([UserRoutes])
 
 module.exports = router
