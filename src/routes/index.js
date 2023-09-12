3cc243523e4b66d8756df2b14b28c5caa52e93d5
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-12 20:12:41 -0300

    rotas , index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const UserRoutes = require("./v1/user.routes");
 const unidadesRoutes = require('./v1/unidades');
+const geracaoRoutes = require('./v1/geracao.routes');
 
 router.use([unidadesRoutes, UserRoutes])
+router.use([geracaoRoutes])
 
 module.exports = router
