8fc1518c35b8925b228f1a279d6d22c2c0dd5646
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-12 21:41:02 -0300

    rotas , get , post , put e delete

--- src/controllers/geracao.controller.js
+++ src/controllers/geracao.controller.js
@@ -28,6 +28,7 @@ module.exports = {
   async createGeracao(req, res) {
     try {
       const { unidade_id, reference_date, total_generated } = req.body;
+      
       if (!unidade_id || !reference_date || !total_generated) {
         return res
           .status(400)
@@ -40,40 +41,40 @@ module.exports = {
     }
   },
 
-    async updateGeracao(req, res) {
-        try {
-          const { id } = req.params;
-          const [updated] = await GeracaoMensal.update(req.body, {
-            where: { id: id },
-          });
-          if (updated) {
-            const updatedGeracao = await GeracaoMensal.findOne({
-              where: { id: id },
-            });
-            return res.status(200).json(updatedGeracao);
-          }
-          throw new Error("Registro não encontrado");
-        } catch (error) {
-          return res.status(500).json({ error: error.message });
-        }
-      },
-
-    async deleteGeracao(req, res) {
-        try {
-          const { id } = req.params;
-          
-            if (!id) {
-                return res.status(400).json({ error: "Id inválido" });
-                }
-          const deleted = await GeracaoMensal.destroy({
-            where: { id: id },
-          });
-          if (deleted) {
-            return res.status(204).send("Registro deletado");
-          }
-          throw new Error("Registro não encontrado");
-        } catch (error) {
-          return res.status(500).json({ error: error.message });
-        }
+  async updateGeracao(req, res) {
+    try {
+      const { id } = req.params;
+      const [updated] = await GeracaoMensal.update(req.body, {
+        where: { id: id },
+      });
+      if (updated) {
+        const updatedGeracao = await GeracaoMensal.findOne({
+          where: { id: id },
+        });
+        return res.status(200).json(updatedGeracao);
       }
+      throw new Error("Registro não encontrado");
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
+
+  async deleteGeracao(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ error: "Id inválido" });
+      }
+      const deleted = await GeracaoMensal.destroy({
+        where: { id: id },
+      });
+      if (deleted) {
+        return res.status(204).send("Registro deletado");
+      }
+      throw new Error("Registro não encontrado");
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
 };
--- src/routes/v1/geracao.routes.js
+++ src/routes/v1/geracao.routes.js
@@ -2,6 +2,10 @@ const geracaoRoutes = require("express").Router();
 const geracaoController = require("../../controllers/geracao.controller");
 
 geracaoRoutes.get("/api/v1/geracao", geracaoController.getGeracao);
+geracaoRoutes.get("/api/v1/geracao/:unidadeId", geracaoController.getGeracaoUnidade);
+geracaoRoutes.post("/api/v1/geracao", geracaoController.createGeracao);
+geracaoRoutes.put("/api/v1/geracao/:id", geracaoController.updateGeracao);
+geracaoRoutes.delete("/api/v1/geracao/:id", geracaoController.deleteGeracao);
 
 module.exports = geracaoRoutes;
 
