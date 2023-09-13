c2c279216a6b9f615ea827d70c567d9e56e912cf
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-12 21:17:00 -0300

    ajuste feito , rota get funcionando

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const UserRoutes = require("./v1/user.routes");
 const unidadesRoutes = require('./v1/unidades');
 const geracaoRoutes = require('./v1/geracao.routes');
 
-//router.use(unidadesRoutes);
-//router.use(UserRoutes);
+router.use(unidadesRoutes);
+router.use(UserRoutes);
 router.use(geracaoRoutes);
 
 module.exports = router;
