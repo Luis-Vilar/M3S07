9fc6949c2381c1808155071919dd91973efb1ac7
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-09-13 19:31:21 +0000

    Fix : Delete middleware Auth no login de usuario

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -15,6 +15,6 @@ userRoutes.get("/api/v1/getOneUser/:id", auth, getOneUser);
 userRoutes.get("/api/v1/getAllUsers", auth, getAllUsers);
 userRoutes.delete("/api/v1/deleteOneUser/:id", auth, deleteOneUser);
 userRoutes.put("/api/v1/updateOneUser/:id", auth, updateOneUser);
-userRoutes.get("/api/v1/userLogin", auth, userLogin);
+userRoutes.get("/api/v1/userLogin", userLogin);
 
 module.exports = userRoutes;
