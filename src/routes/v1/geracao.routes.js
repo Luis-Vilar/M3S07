6435344bb1237fdaa5764e5ec7eb49d6d3db8aa8
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-13 22:20:47 -0300

    adicionado o auth nas rotas geracaoRoutes

--- src/routes/v1/geracao.routes.js
+++ src/routes/v1/geracao.routes.js
@@ -1,11 +1,12 @@
 const geracaoRoutes = require("express").Router();
 const geracaoController = require("../../controllers/geracao.controller");
+const { auth } = require("../../middleware/auth");
 
-geracaoRoutes.get("/api/v1/geracao", geracaoController.getGeracao);
-geracaoRoutes.get("/api/v1/geracao/:unidadeId", geracaoController.getGeracaoUnidade);
-geracaoRoutes.post("/api/v1/geracao", geracaoController.createGeracao);
-geracaoRoutes.put("/api/v1/geracao/:id", geracaoController.updateGeracao);
-geracaoRoutes.delete("/api/v1/geracao/:id", geracaoController.deleteGeracao);
+geracaoRoutes.get("/api/v1/geracao", auth, geracaoController.getGeracao);
+geracaoRoutes.get("/api/v1/geracao/:unidadeId", auth ,geracaoController.getGeracaoUnidade);
+geracaoRoutes.post("/api/v1/geracao", auth, geracaoController.createGeracao);
+geracaoRoutes.put("/api/v1/geracao/:id", auth , geracaoController.updateGeracao);
+geracaoRoutes.delete("/api/v1/geracao/:id", auth , geracaoController.deleteGeracao);
 
 module.exports = geracaoRoutes;
 
