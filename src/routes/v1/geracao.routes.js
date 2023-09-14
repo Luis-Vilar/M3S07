d772cc8995ccfa21015e646d178d77bc9988dc4f
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-14 12:07:57 -0300

    adicionado os auth que faltou

--- src/routes/v1/geracao.routes.js
+++ src/routes/v1/geracao.routes.js
@@ -4,8 +4,8 @@ const { auth } = require("../../middleware/auth");
 
 geracaoRoutes.get("/api/v1/geracao", auth, geracaoController.getGeracao);
 geracaoRoutes.get("/api/v1/geracao/:unidadeId", auth ,geracaoController.getGeracaoUnidade);
-geracaoRoutes.post("/api/v1/geracao",  geracaoController.createGeracao);
-geracaoRoutes.put("/api/v1/geracao/:id",  geracaoController.updateGeracao);
+geracaoRoutes.post("/api/v1/geracao", auth,  geracaoController.createGeracao);
+geracaoRoutes.put("/api/v1/geracao/:id", auth , geracaoController.updateGeracao);
 geracaoRoutes.delete("/api/v1/geracao/:id", auth , geracaoController.deleteGeracao);
 
 module.exports = geracaoRoutes;
