b6e9ca51644b5063aeec897658ea6d94689addc1
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-21 20:25:53 -0300

    adicionando morgan-body /  configurando morgan

--- src/server.js
+++ src/server.js
@@ -2,10 +2,18 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const morganBody = require('morgan-body');
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("../swagger"); 
 const { config } = require("dotenv");
 config();
+
+const path = require('path')
+const fs = require('fs');
+
+const log = fs.createWriteStream(
+path.join(__dirname, "./logs", `express.log`), { flags: "a" }
+);
 
 // classe server
 class Server {
@@ -21,9 +29,12 @@ class Server {
     app.use(cors());
     app.use(express.json());
     app.use(morgan("dev"))
-    
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+    morganBody(app, {
+      noColors: true, // Desabilita cores no console (optional)
+      stream: process.stdout, // Define onde será realizada a saída das informações (optional)
+      });
+      
   }
   // connect database
   async database() {
