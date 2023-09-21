670f4c959214ed8f0768fcd2ece8551768f6260e
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-21 19:56:16 -0300

    ajuste no auth js , para usar Bearer para usar o token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,14 +5,16 @@ config();
 async function auth(req, res, next) {
   try {
     const { authorization } = req.headers;
-    if (!authorization) {
+    if (!authorization || !authorization.startsWith("Bearer ")) {
       return res.status(401).send({
         message: "Autenticação Falhou",
-        cause: "Token não informado",
+        cause: "Token não informado ou em formato inválido",
       });
     }
-    req["payload"] = verify(authorization, process.env.SECRET_JWT);
-    console.log(req["payload"]);
+    const token = authorization.slice(7); // Remove o prefixo "Bearer " do token
+    req.headers.authorization = token; // Atribui o token JWT ao cabeçalho "Authorization"
+    req.payload = verify(token, process.env.SECRET_JWT);
+    console.log(req.payload);
     next();
   } catch (error) {
     return res.status(401).send({
