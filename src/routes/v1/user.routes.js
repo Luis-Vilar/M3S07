9725bf2a8f96d58c3a9010b115489fa8be759acc
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-21 15:34:45 -0300

    ajuste swagger

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,6 +1,4 @@
 const userRoutes = require("express").Router();
-const { auth } = require("../../middleware/auth");
-
 const {
   createUser,
   getAllUsers,
@@ -8,13 +6,141 @@ const {
   deleteUser,
   login,
 } = require("../../controllers/user.controller");
+const { auth } = require("../../middleware/auth");
 
+/**
+ * @swagger
+ * /api/v1/login:
+ *   post:
+ *     summary: Fazer login.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido.
+ */
 userRoutes.post("/api/v1/login", login);
-userRoutes.get("/api/v1/usuario",  getAllUsers);
 
-userRoutes.post("/api/v1/usuario",  createUser);
+
+/**
+ * @swagger
+ * /api/v1/usuario:
+ *   get:
+ *     summary: Retorna todos os usuários.
+ *     security:
+ *       - jwtAuth: []  # Autenticação JWT é necessária
+ *     responses:
+ *       200:
+ *         description: Sucesso.
+ */
+userRoutes.get("/api/v1/usuario", auth, getAllUsers);
+
+
+/**
+ * @swagger
+ * /api/v1/usuario:
+ *   get:
+ *     summary: Retorna todos os usuários.
+ *     security:
+ *       - jwtAuth: []  # Autenticação JWT é necessária
+ *     responses:
+ *       200:
+ *         description: Sucesso.
+ *   post:
+ *     summary: Cria um novo usuário.
+ *     security:
+ *       - jwtAuth: []  # Autenticação JWT é necessária
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso.
+ */
+userRoutes.post("/api/v1/usuario", createUser);
+
+/**
+ * @swagger
+ * /api/v1/usuario/{id}:
+ *   put:
+ *     summary: Atualiza um usuário existente.
+ *     security:
+ *       - jwtAuth: []  # Autenticação JWT é necessária
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID do usuário a ser atualizado.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso.
+ */
+
 userRoutes.put("/api/v1/usuario/:id", auth, updateUser);
 
+/**
+ * @swagger
+ * /api/v1/usuario/{id}:
+ *   delete:
+ *     summary: Exclui um usuário existente.
+ *     security:
+ *       - jwtAuth: []  # Autenticação JWT é necessária
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID do usuário a ser excluído.
+ *     responses:
+ *       204:
+ *         description: Usuário excluído com sucesso.
+ */
 userRoutes.delete("/api/v1/usuario/:id", auth, deleteUser);
 
 module.exports = userRoutes;
