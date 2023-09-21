a5bfcc9a003e28bcac8b3f2c82f3120139883ce7
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-21 20:52:51 -0300

    adicionando morgan-body /  configurando morgan

--- swagger.js
+++ swagger.js
@@ -31,11 +31,11 @@ const swaggerSpec = swaggerJsdoc(options);
 delete swaggerSpec.paths.default;
 
 //Usuarios
-swaggerSpec.paths["/api/v1/login"]["post"]["tags"] = ["Usuarios"];
-swaggerSpec.paths["/api/v1/usuario"]["get"]["tags"] = ["Usuarios"];
-swaggerSpec.paths["/api/v1/usuario"]["post"]["tags"] = ["Usuarios"];
-swaggerSpec.paths["/api/v1/usuario/{id}"]["put"]["tags"] = ["Usuarios"];
-swaggerSpec.paths["/api/v1/usuario/{id}"]["delete"]["tags"] = ["Usuarios"];
+swaggerSpec.paths["/api/v1/login"]["post"]["tags"] = ["Usuários"];
+swaggerSpec.paths["/api/v1/usuario"]["get"]["tags"] = ["Usuários"];
+swaggerSpec.paths["/api/v1/usuario"]["post"]["tags"] = ["Usuários"];
+swaggerSpec.paths["/api/v1/usuario/{id}"]["put"]["tags"] = ["Usuários"];
+swaggerSpec.paths["/api/v1/usuario/{id}"]["delete"]["tags"] = ["Usuários"];
 
 // Personalize o título "Default" aqui unidades
 swaggerSpec.paths["/api/v1/unidades"]["get"]["tags"] = ["Unidades"];
@@ -44,10 +44,10 @@ swaggerSpec.paths["/api/v1/unidades/{id}"]["put"]["tags"] = ["Unidades"];
 swaggerSpec.paths["/api/v1/unidades/{id}"]["delete"]["tags"] = ["Unidades"];
 
 // Personalize o título "Default" aqui
-swaggerSpec.paths["/api/v1/geracao"]["get"]["tags"] = ["Geracao"]; 
-swaggerSpec.paths["/api/v1/geracao"]["post"]["tags"] = ["Geracao"]; 
-swaggerSpec.paths["/api/v1/geracao/{unidadeId}"]["get"]["tags"] = ["Geracao"];
-swaggerSpec.paths["/api/v1/geracao/{id}"]["put"]["tags"] = ["Geracao"];
-swaggerSpec.paths["/api/v1/geracao/{id}"]["delete"]["tags"] = ["Geracao"];
+swaggerSpec.paths["/api/v1/geracao"]["get"]["tags"] = ["Geração"]; 
+swaggerSpec.paths["/api/v1/geracao"]["post"]["tags"] = ["Geração"]; 
+swaggerSpec.paths["/api/v1/geracao/{unidadeId}"]["get"]["tags"] = ["Geração"];
+swaggerSpec.paths["/api/v1/geracao/{id}"]["put"]["tags"] = ["Geração"];
+swaggerSpec.paths["/api/v1/geracao/{id}"]["delete"]["tags"] = ["Geração"];
 
 module.exports = swaggerSpec;
